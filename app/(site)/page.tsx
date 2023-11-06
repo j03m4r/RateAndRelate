0ce4ccf4bdc28234ed81757cff3a6adac39cfee0
j03m4r <joeemarr12345@gmail.com>
2023-11-05 20:51:47 -0600

    Cleaning up unused imports and fixed a navigation bug

--- app/(site)/page.tsx
+++
@@ -1,9 +0,0 @@
-import LandingPageContent from "./components/LandingPageContent";
-
-export const revalidate = 0;
-
-export default function LandingPage() {
-  return (
-    <LandingPageContent />
-  )
-}
