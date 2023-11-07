c9ac6b93c9f1a3c6d23f6de3bc4ac38f1cff8276
j03m4r <joeemarr12345@gmail.com>
2023-11-07 12:34:36 -0600

    Adjusting rating cycle to start at 12am the day of.

--- libs/DateCalculations.ts
+++ libs/DateCalculations.ts
@@ -1,6 +1,6 @@
 export const calculatePreviousDate = (today: Date) => {
-    const prevDay = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 17, 0, 0, 0);
-    if (today.getHours() < 17) {
+    const prevDay = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 6, 0, 0, 0);
+    if (today.getHours() < 6) {
         if (today.getDate() === 1) {
             if (today.getMonth() === 0) {
                 prevDay.setFullYear(today.getFullYear() - 1);
