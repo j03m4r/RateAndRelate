902e62f1523c6b20dcfb259f3909403dcf6d3195
j03m4r <joeemarr12345@gmail.com>
2023-11-07 14:56:58 -0600

    Fixing padding between ratings on profile page.

--- app/profile/[id]/components/ProfileRatingFeed.tsx
+++ app/profile/[id]/components/ProfileRatingFeed.tsx
@@ -9,7 +9,7 @@ const ProfileRatingFeed: React.FC<ProfileRatingFeedProps> = ({ profile }) => {
     return (
         <div>
             {profile.ratings ? (
-                <div className="flex flex-col-reverse items-center w-full gap-y-10 md:gap-y-16"> {/* Today's ratings. Twitter-like feed */}
+                <div className="flex flex-col-reverse items-center w-full gap-y-14 md:gap-y-16"> {/* Today's ratings. Twitter-like feed */}
                     {profile.ratings.map((rating) => (
                         <RatingCard key={rating.id} rating={rating} n={0} />
                     ))}
