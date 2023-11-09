efba820ff342a8c93294ffeed4f6471bee215a49
j03m4r <joeemarr12345@gmail.com>
2023-11-09 15:28:08 -0600

    Changed web browser icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,9 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link rel='icon' href='/favicon.ico'/>
+      </head>
       <body className={font.className}>
         <ToasterProvider />
         <SupabaseProvider>
